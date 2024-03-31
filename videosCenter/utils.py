1eef7ea7eaa0936a97168626b32640b6a7724848
Sumit Dey <sumitdeyodisha@gmail.com>
2024-03-31 16:47:07 +0530

    Updated the fetching data approach i.e. now in every 10 second latest data is coming according to the predefined query and if data is finished or broken for some internal issues then it changes to next predefined query. if the api key is out of it's quota then it change to another API. it keeps changing itself to next probable query or key if any problem occues during the data fetching.

--- videosCenter/utils.py
+++ videosCenter/utils.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 import requests
 from requests.exceptions import RequestException, HTTPError, ConnectionError
 from django.conf import settings
@@ -6,18 +7,18 @@ from django.conf import settings
 
 class GetLatestVideos:
 
-    # it fetches the current time
-    current_time = settings.CURRENT_TIME.strftime("%Y-%m-%dT%H:%M:%SZ")
-
-    # queries
-    query_params = {
-        "part": "snippet",
-        "order": "date",
-        "publishafter": current_time,
-        "type": "video",
-        "q": "gaming",
-        "key": settings.API_KEY,
-    }
+    def __init__(self) -> None:
+        # queries
+        self.query_params = {
+            "part": "snippet",
+            "order": "date",
+            "type": "video",
+            "q": "vlogging",
+            "key": settings.API_KEYS[0],
+            "publishafter": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+        }
+        self.query_length = len(settings.PREDEFINED_SEARCH_QUERIES)
+        self.search_idx = self.api_key_idx = 0
 
     def get_videos(self):
         try:
@@ -25,8 +26,48 @@ class GetLatestVideos:
             response = requests.get(
                 url=f"{settings.URL}/search", params=self.query_params
             )
-            # return the response
-            return {"code": response.status_code, "data": response.json()}
+            if response.status_code == 200:
+                response = response.json()
+                next_page = response.get("nextPageToken")
+                data = []
+
+                if next_page:
+                    self.query_params["pageToken"] = next_page
+                else:
+                    self.search_idx = (self.search_idx + 1) % self.query_length
+                    self.query_params["q"] = settings.PREDEFINED_SEARCH_QUERIES[
+                        self.search_idx
+                    ]
+                # fetching required data i.e. title, description, published datetime, thumbnails
+                for item in response.get("items"):
+                    if item:
+                        video_data = {}
+                        item_details = item.get("snippet")
+
+                        # loading datas
+                        title = item_details.get("title")
+                        description = item_details.get("description")
+                        thumbnails = item_details.get("thumbnails")
+                        publish_time = item_details.get("publishTime")
+
+                        video_data["title"] = title
+                        video_data["description"] = description
+                        video_data["thumbnails"] = thumbnails
+                        video_data["published_time"] = publish_time
+                        data.append(video_data)
+                # return the required data
+                return {"status": "sucess", "next_page": next_page, "data": data}
+            elif response.status_code == 403:
+                response = response.json()
+                error_msg = response.get("error").get("message")
+                if "exceed" in error_msg:
+                    self.api_key_idx = (self.api_key_idx + 1) % len(settings.API_KEYS)
+                    self.query_params["key"] = settings.API_KEYS[self.api_key_idx]
+            return {
+                "code": response.status_code,
+                "status": "failed",
+                "error_msg": response.json(),
+            }
         except (
             # incase of internal failuers to get response
             RequestException,
