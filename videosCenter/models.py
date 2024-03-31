0c26e19bf1b18bf06fa88211258a0724c44c2a7c
Sumit Dey <sumitdeyodisha@gmail.com>
2024-03-31 17:12:01 +0530

    Tansfering the database scemas into model

--- videosCenter/models.py
+++ videosCenter/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
 
+class Videos(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=5000)
+    published_time = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f"{self.title}-{self.published_time}"
+
+
 class Thumbnails(models.Model):
     url = models.URLField()
     width = models.IntegerField()
     height = models.IntegerField()
+    video = models.ForeignKey(
+        Videos, on_delete=models.CASCADE, related_name="thumnails"
+    )
 
-class Videos(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.CharField(max_length=5000)
-    publishing_datetime = models.DateTimeField()
-    thumbnails = models.OneToOneField(Thumbnails, on_delete=models.CASCADE, related_name='videos_img')
+    def __str__(self) -> str:
+        return f"{self.video}-{self.url}"
