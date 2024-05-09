fe4871832c6e71c3a61cc9b6f2248a0f12f3b3db
Sumit Dey <sumitdeyodisha@gmail.com>
2024-05-10 00:12:38 +0530

    Added home page

--- videosCenter/urls.py
+++ videosCenter/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import VideoView
 
 urlpatterns = [
     path("videos/", view=VideoView.as_view(), name="dummy"),
+    path("", view=TemplateView.as_view(template_name="Home.html"), name="home-page"),
 ]
